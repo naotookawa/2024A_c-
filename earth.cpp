1cbc960da66802067f52c56eeb0bf3c70d7c86f7
naotookawa <na.okawa04@gmail.com>
2024-10-21 10:53:53 +0900

    create remote repo

---
+++ earth.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 4.0 * 3.14159265358979 * 6371000.0 * 6371000.0 << endl;
+}
---
+++ earth_example4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 4.0 * 3.14159265358979 * 6371000.0 * 6371000.0 << endl;
+    cout << "4.0 * 3.14159265358979 * 6371000.0 * 6371000.0" << endl;
+}
